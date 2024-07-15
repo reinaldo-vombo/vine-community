d8ff208c4722c3f6e14aae1fc5b809d3473ef4da
Salomão Nunes <reinaldo.vombo@gmail.com>
2024-07-15 15:42:06 +0100

    fix OrganizationSwitcherand remove google and email from lognin

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,4 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from '@clerk/nextjs'
-import { dark } from '@clerk/themes'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -22,14 +21,7 @@ function Topbar() {
 					</SignedIn>
 				</div>
 
-				<OrganizationSwitcher
-					appearance={{
-						baseTheme: dark,
-						elements: {
-							organizationSwitcherTrigger: 'py-2 px-4',
-						},
-					}}
-				/>
+				<OrganizationSwitcher />
 			</div>
 		</nav>
 	)
